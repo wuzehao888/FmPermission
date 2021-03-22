460a3f4d2b6e4e94aa1edac41d72c5ee5311fb1e
WuMoneyException <719691343@qq.com>
2021-03-22 11:14:05 +0800

    commit FmPermission

--- fmpermission/src/androidTest/java/com/wzh/fmpermission/ExampleInstrumentedTest.kt
+++ fmpermission/src/androidTest/java/com/wzh/fmpermission/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.wzh.fmpermission", appContext.packageName)
+        assertEquals("com.wzh.fmpermission.test", appContext.packageName)
     }
 }
